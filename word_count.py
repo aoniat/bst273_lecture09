d0a7edffe4a72f81d7c434937f14d68ea3078523
aoniat <aonia_traxler@hsph.harvard.edu>
2018-10-04 12:28:41 -0400

    open and read lines of file

--- word_count.py
+++ word_count.py
@@ -17,6 +17,15 @@ parser.add_argument(
 args = parser.parse_args( )
 print(args)
 print(args.data_file)
+
+fh= open(args.data_file) #want to assign opening to
+print("the file handle is", fh)
+
+lines= 0 #want to return a number so set the lists to zero
+words= 0
+chars= 0
 #-------------------------------------------------------------------------------
 # our code for analyzing the data
 #-------------------------------------------------------------------------------
+for line in fh:
+	print(line)
